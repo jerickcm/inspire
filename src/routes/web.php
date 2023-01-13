e8714005d72d2fae7afb5bf4ea1393f36b0134ef
Jerick-CM <jmangaluz@gmail.com>
2023-01-13 10:49:34 +0800

    Initial commit

---
+++ src/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Fhsinchy\Inspire\Controllers;
+use Illuminate\Support\Facades\Route;
+
+Route::get('inspire', Fhsinchy\Inspire\Controllers\InspirationController::class);
+// Route::get('inspire', InspirationController::class);
