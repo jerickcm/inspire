31204a5249573a0a224ab3a68b485c843f562b15
Jerick-CM <jmangaluz@gmail.com>
2023-01-17 09:44:32 +0800

    fix folder name

--- src/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Fhsinchy\Inspire\Controllers;
-use Illuminate\Support\Facades\Route;
-
-Route::get('inspire', Fhsinchy\Inspire\Controllers\InspirationController::class);
-// Route::get('inspire', InspirationController::class);
-
