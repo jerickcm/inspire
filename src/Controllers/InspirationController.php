f5184805b1b0d6842b3a31516b6c157ebae32b8b
Jerick-CM <jmangaluz@gmail.com>
2023-01-17 13:58:45 +0800

    update

--- src/Controllers/InspirationController.php
+++ src/Controllers/InspirationController.php
@@ -9,6 +9,7 @@ class InspirationController
 {
     public function __invoke(Inspire $inspire)
     {
+
         $quote = $inspire->justDoIt();
         return view('inspire::index', compact('quote'));
     }
