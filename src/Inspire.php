254fad5cfa258dc39deff855f5290c1f7f90cf5c
Jerick-CM <jmangaluz@gmail.com>
2023-02-02 08:46:58 +0800

    push edited version 4.2.2

--- src/Inspire.php
+++ src/Inspire.php
@@ -9,6 +9,7 @@ class Inspire
     public function justDoIt()
     {
         // testchange
+
         $response = Http::get('https://inspiration.goprogram.ai/');
         return $response['quote'] . ' -' . $response['author'];
     }
